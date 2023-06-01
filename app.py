5aa39376e027a144a5f64b0659c17407788e52c2
Dami Elulade <dme21@ic.ac.uk>
2023-06-02 00:55:48 +0100

    Restructure application directories

--- app.py
+++ app.py
@@ -1,28 +1,5 @@
-from flask import Flask, request, render_template
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/hello')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-@app.route('/calc')
-def calc():
-    a = int(request.args.get('a'))
-    b = int(request.args.get('b'))
-    return str(sum_two(a, b))
-
-
-def sum_two(a, b):
-    return a + b
-
+from src import app
 
 if __name__ == '__main__':
     app.run(debug=True)
+

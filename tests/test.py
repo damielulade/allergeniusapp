c5bd150defb2a4b017c9cb6cabb5232dc7b7f030
Dami Elulade <dme21@ic.ac.uk>
2023-06-02 14:17:01 +0100

    Edit test.py and add new test firebase_test.py

--- tests/test.py
+++ tests/test.py
@@ -11,18 +11,3 @@ class Test(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
-
-# import unittest
-# from routes import sum_two
-#
-#
-# class Test(unittest.TestCase):
-#
-#     def test_calc(self):
-#         value = sum_two(4, 5)
-#         self.assertEquals(value, 9)
-#
-#
-# if __name__ == '__main__':
-#     unittest.main()

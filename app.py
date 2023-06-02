dde81f6bba0a4cddf6c67de8b61804072414f9ed
Thatcher Ference <tbf20@ic.ac.uk>
2023-06-02 02:26:55 +0100

    Added placeholder app with example map and filter
    - Thatcher Ference <tbf20>

--- app.py
+++ app.py
@@ -1,23 +1,17 @@
-from flask import Flask, request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-@app.route('/calc')
-def calc():
-    a = int(request.args.get('a'))
-    b = int(request.args.get('b'))
-    return str(sum_two(a, b))
-
-
-def sum_two(a, b):
-    return a + b
-
+@app.route("/")
+def main():
+    return render_template("index.html")
+    
+@app.route('/filter')
+def filter():
+    return render_template('filter.html')
 
 if __name__ == '__main__':
     app.run()
+
+
+

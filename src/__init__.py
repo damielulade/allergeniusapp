06aaba1861e780e9db0dbecdeef8a0fafe5f34ab
Hyunjun Choi <hyunjun.choi21@imperial.ac.uk>
2023-06-06 01:21:36 +0100

    Blanks project

--- src/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-import os
-
-app = Flask(__name__, static_folder='static', template_folder='templates')
-app.config['SECRET_KEY']='blablabla'
-
-APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-from src import routes

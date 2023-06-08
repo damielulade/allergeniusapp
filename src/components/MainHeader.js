745b273c9aeefa711e2c2fe97e471b946d7597c6
Thatcher Ference <tbf20@ic.ac.uk>
2023-06-08 23:18:03 +0100

    Fixed settings button on mobile - the unicode gear symbol was rendering as an emoji on mobile
    - Thatcher Ference <tbf20>

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import settings from "../static/images/settings.png";
 
 export default function MainHeader() {
   return (
     <div className = "top-bar">
       <Link to="/" id = "back-button"><span>〈</span></Link>
       <div id="top-title">Allergenius</div>
-      <Link to="/settings" id = "settings-button"><span>⚙</span></Link>
+      <Link to="/settings" id = "settings-button"><img src={settings} id = "settings-img" alt = "temp"/></Link>
     </div>
   )
 }

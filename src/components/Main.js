940e6be8d74a2c1a3f2612d7e07489d208d32b39
Thatcher Ference <tbf20@ic.ac.uk>
2023-06-08 14:02:06 +0100

    Begin work on fixing search page - Placeholder query to demonstrate functionality
    - Thatcher Ference <tbf20>

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,15 +6,24 @@ import accountimg from "../static/images/account.png";
 import MainHeader from "./MainHeader";
 
 export default function Main() {
-  return (    
-		<div className = "main">
-			<MainHeader />	
-			<div className = "container-index">
+    function exampleQuery() {
+        const query = document.getElementById("search-input").value;
+        if (query === "american"){
+            window.location.href = 'search';
+        } else {
+            window.location.href = 'search';
+        }
+    }
+
+    return (
+        <div className = "main">
+            <MainHeader />
+            <div className = "container-index">
 				<img src={mapimg} id="map-img" alt = "temp" />
                 <div className="map-block">
                     <div id="search-box">
                         <input type="text" placeholder="Search..." id = "search-input" />
-                        <button onClick = "exampleQuery()" id = "search-button"><span>⌕</span></button>
+                        <button onClick ={exampleQuery} id = "search-button"><span>⌕</span></button>
                         {/* @TODO: fix/replace this button  */}
                     </div>
                     <Link to="/filter" id="filter-button">
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -3,12 +3,20 @@ import searchresults1 from "../static/images/searchresults1.png";
 import MainHeader from "../components/MainHeader";
 
 export default function SearchPage() {
+    const data = [
+        { val: 'Value 1' },
+        { val: 'Value 2' },
+        { val: 'Value 3' },
+    ];
   return (
       <div className = "section">
           <div class = "main">
               <MainHeader />
               <div className="container-other">
-                  <img src={searchresults1} id="searchresults" alt = "temp"/>
+                  {data.map((r, index) => (
+                      <p key={index}>{r.val}</p>
+                  ))}
+                  {/*<img src={searchresults1} id="searchresults" alt = "temp"/>*/}
               </div>
           </div>
       </div>

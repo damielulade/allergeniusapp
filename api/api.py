7073d76a9483acec574482fa4d8bcc05a9cec4b7
Dami Elulade <dme21@ic.ac.uk>
2023-06-08 17:08:02 +0100

    Add flask_cors to requirements

--- api/api.py
+++ api/api.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 from firebase import add_user, get_values, db
+import json
 
 app = Flask(__name__, static_folder="../build", static_url_path='/')
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
 def index():
@@ -16,8 +20,13 @@ def example():
     add_user({"firstName": "Brett", "lastName": "Conway", "Age": 20})
     add_user({"firstName": "Brett", "lastName": "Conroute", "Age": 20})
     add_user({"firstName": "Brett", "lastName": "Connection", "Age": 20})
-    return get_values(db.child("user"))
+    return json.dumps(get_values(db.child("user")))
     # assert(false)
+    
+@app.route('/collectData', methods=["GET"])
+@cross_origin()
+def foo():
+    return db.child("user").get().val()
     
 if __name__ == '__main__':
     app.run(debug=True)

c37861724417380060578708f093b974f3756903
Thatcher Ference <tbf20@ic.ac.uk>
2023-06-12 16:18:23 +0100

    Fixed race condition issue for maps API - removed script tag from index.html - issue "Google Maps JavaScript API being loaded without a callback" resolved
    - Thatcher Ference <tbf20>

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,4 +1,5 @@
-import React, {useEffect, useRef, useMemo} from 'react';
+import React, { useEffect, useRef, useMemo } from 'react';
+import { Loader } from "@googlemaps/js-api-loader"
 
 export default function MapComponent() {
     const mapContainerRef = useRef(null);
@@ -9,30 +10,32 @@ export default function MapComponent() {
         fullscreenControl: false,
         streetViewControl: false,
         keyboardShortcuts: false,
-        // zoomControlOptions: {
-        //     position: window.google.maps.ControlPosition.BOTTOM_LEFT,
-        // },
     }), []);
 
     const center = useMemo(() => ({
         lat: 51.5074,
         lng: -0.1278,
-        // lat: 51.4018,
-        // lng: -0.079,
     }), []);
 
     useEffect(() => {
-        if (!mapInstanceRef.current) {
-            // Load the map for the first time
-            mapInstanceRef.current = new window.google.maps.Map(mapContainerRef.current, {
-                center: center,
-                options: options,
-                zoom: 10,
-            });
-        }
+        const loader = new Loader({
+            apiKey: "AIzaSyA5lTihboPl_N7Yt8T3worfrbjvF1MDLWc",
+            version: "weekly",
+            libraries: ["places"]  // Specify the libraries you need
+        });
+
+        loader.load().then(() => {
+            if (!mapInstanceRef.current) {
+                // Load the map for the first time
+                mapInstanceRef.current = new window.google.maps.Map(mapContainerRef.current, {
+                    center: center,
+                    options: options,
+                    zoom: 10,
+                });
+            }
+        });
 
         // Clean up the map instance on component unmount
-
         return () => {
             if (mapInstanceRef.current) {
                 mapInstanceRef.current = null;
@@ -47,5 +50,5 @@ export default function MapComponent() {
     );
 }
 
-
 // AIzaSyA5lTihboPl_N7Yt8T3worfrbjvF1MDLWc
+//<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA5lTihboPl_N7Yt8T3worfrbjvF1MDLWc&libraries=places"></script>

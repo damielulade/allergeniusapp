2e2018367f6198ffc00d42b737be91de7c54cccc
Dami Elulade <dme21@ic.ac.uk>
2023-06-13 03:11:21 +0100

    Start implementing live db updates for allergens

--- src/components/AllergenCard.js
+++ src/components/AllergenCard.js
@@ -3,9 +3,24 @@ import axios from "axios";
 import userimg from "../static/images/user.png";
 
 export default function AllergenCard(props) {
+  const [checked, setChecked] = useState(false);
+  // this state needs to be changed to take the db value for the allergen
+
+  const handleChange = () => {
+    setChecked(!checked);
+    // update the db to set this allergen
+  };
+
   return (
     <label className="checkbox-container" htmlFor={props.id}>
-      <input type="checkbox" id={props.id} name={props.id} value={props.allergen} />
+      <input
+        type="checkbox"
+        id={props.id}
+        name={props.id}
+        value={props.allergen}
+        checked={checked}
+        onChange={handleChange}
+      />
       <span className="checkmark"></span> {props.allergen}
     </label>
   );

bbd6763aed0462eaddf1b2a45eecb85d5ca28a00
Dami Elulade <dme21@ic.ac.uk>
2023-06-15 18:14:08 +0100

    Start implementing friends logic in front end

--- src/components/FriendsCard.js
+++ src/components/FriendsCard.js
@@ -1,8 +1,17 @@
 import React, { useEffect, useState } from "react";
-import axios from 'axios'
+import axios from "axios";
 import userimg from "../static/images/user.png";
 
 export default function FriendsCard(props) {
+  const [dropdownState, setDropdownState] = useState(false);
+
+  const viewDetails = () => {
+    setDropdownState(!dropdownState);
+  };
+
+  const allergens = props.info.allergens.map((allergen) => {
+    return <p>- {allergen}</p>;
+  });
 
   return (
     <div id="friend1">
@@ -10,9 +19,22 @@ export default function FriendsCard(props) {
         <img src={userimg} id="friends-page-img" alt="temp" />
       </div>
       <div id="friend-description-box">
-        <p id="friend1-name">{props.firstName} {props.lastName}</p>
-        <p id="friends-desc">Allergens: {props.allergens}</p>
+        <p id="friend1-name">
+          {props.info.firstName} {props.info.lastName}
+        </p>
+        <p id="friends-desc">{props.info.email}</p>
       </div>
+      {props.info.privacy && (
+        <div id="friend-delete-button" onClick={viewDetails}>
+          <p>see allergens</p>
+        </div>
+      )}
+      {dropdownState && (
+        <div>
+          <p id="friends-desc">Allergens</p>
+          <div id="friends-desc">{allergens}</div>
+        </div>
+      )}
       {/* <img src={userimg} id="userimg1" /> */}
     </div>
   );
--- src/pages/FriendsPage.js
+++ src/pages/FriendsPage.js
@@ -5,51 +5,75 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 
 export default function FriendsPage() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({});
 
   useEffect(() => {
     const fetchData = () => {
       axios
-          .get(`/api/getUserFriends`)
-          .then((response) => {
-            setData(response.data);
-          })
-          .catch((error) => console.log(error));
+        .get(`/api/friends`)
+        .then((response) => setData(response.data))
+        .catch((error) => console.log(error));
     };
     fetchData();
+    // console.log(data)
   }, []);
 
-  const friends = data.map((user) => {
+  const [temp, setTemp] = useState(null)
+
+  const findFriend = () => {
+    const email = document.getElementById("search-friends-input").value;
+    if (email) {
+      // var user_key;
+      axios
+        .get(`/api/user_by_email/${email}`)
+        .then((response) => {
+          setTemp(response.data);
+          // console.log(response.data);
+        })
+        .catch((error) => console.log(error));
+      if (temp) {
+        console.log(temp)
+        axios
+          .post(`/api/friends`, {
+            mode: "add",
+            friendKey: temp,
+          })
+          .then((response) => setData(response.data))
+          .catch((error) => console.log(error));
+      }
+      // window.location.reload(false); 
+    }
+  };
+
+  var friends = Object.entries(data).map((friend) => {
     return (
       <FriendsCard
-        key={user.firstName}
-        firstName={user.firstName}
-        lastName={user.lastName}
-        allergens={user.allergens}
+        key={friend}
+        id={friend[0]}
+        info={friend[1]}
       />
     );
-  });
+  })  
 
   return (
     <div className="section">
       <div className="main">
         <MainHeaderVariant />
         <div className="container-friends">
-          <h2 id="friends-ID-title">Search for a user by user ID</h2>
+          <h2 id="friends-ID-title">Search for a user by email</h2>
           <div id="search-friends">
             <input
               type="text"
               placeholder="Search..."
               id="search-friends-input"
             />
-            <button id="search-friends-button">
-              <span>⌕</span>
+            <button id="search-friends-button" onClick={findFriend}>
+              {/* <span>⌕</span> */}
+              <span>+</span>
             </button>
           </div>
           <h2 id="friends-title">Existing Friends</h2>
-          <div className = "friends-scrollable">
-            {friends}
-          </div>
+          <div className="friends-scrollable">{friends}</div>
         </div>
         <div className="button-bar">
           <Link to="/friends" id="friends-button">

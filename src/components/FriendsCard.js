5d061f69be6b8b59ce068c6233164331152936c0
Thatcher Ference <tbf20@ic.ac.uk>
2023-06-16 21:58:20 +0100

    Formatting FriendsPage, FriendsCard.js, GroupsPage and GroupCard.js
    - Thatcher Ference <tbf20>

--- src/components/FriendsCard.js
+++ src/components/FriendsCard.js
@@ -1,49 +1,69 @@
 import React, { useState } from "react";
-import userimg from "../static/images/user.png";
+import mark from "../static/images/user_images/mark_long_hair.png";
+import konst from "../static/images/user_images/konst.png";
+import blank_user_img from "../static/images/user.png";
 
 export default function FriendsCard(props) {
-  const [dropdownState, setDropdownState] = useState(false);
+    const [dropdownState, setDropdownState] = useState(false);
+    const viewDetails = () => {
+        setDropdownState(!dropdownState);
+    };
 
-  const viewDetails = () => {
-    setDropdownState(!dropdownState);
-  };
+    const allergens = props.info.allergens?.map((allergen) => {
+        return <li>{allergen}</li>;
+    });
 
-  const allergens = props.info.allergens?.map((allergen) => {
-    return <p>- {allergen}</p>;
-  });
+    const imageMap = {
+        "mark_long_hair": mark,
+        "konst": konst,
+    };
 
-  return (
-    <div id="friend1">
-      <div id="friend-profile-picture">
-        <img src={userimg} id="friends-page-img" alt="temp" />
-      </div>
-      <div id="friend-description-box">
-        <p id="friend1-name">
-          {props.info.firstName} {props.info.lastName}
-        </p>
-        <p id="friends-desc">{props.info.email}</p>
-      </div>
-      {!props.info.privacy && props.displayAllergens && (
-        <div id="friend-delete-button" onClick={viewDetails}>
-          <p>{dropdownState ? "Hide" : "View"} Allergens</p>
-        </div>
-      )}
-      {dropdownState && (
-        <div>
-          {props.info.allergens && (
-            <div>
-              <p id="friends-desc">Allergens</p>
-              <div id="friends-desc">{allergens}</div>
+    function parseImage(userImage) {
+        // console.log(userImage);
+        if (userImage === null || userImage === "" || userImage === "default") {
+            return blank_user_img;
+        } else {
+            return imageMap[userImage];
+        }
+    }
+
+    return (
+        <div className="friend-card">
+            <div className="friend-profile-picture">
+                <img src={parseImage(props.info.userImage)} id="friends-page-img" alt="temp" />
             </div>
-          )}
-          {!props.info.allergens && (
-            <div>
-              <p id="friends-desc">Allergens: NONE SPECIFIED</p>
+            <div className="friend-details">
+                <div className="friend-description-box">
+                    <p className="friend-name">
+                        {props.info.firstName} {props.info.lastName}
+                    </p>
+                    <p className="friends-desc">{props.info.email}</p>
+                </div>
+                <div className="friend-allergen-box">
+                    {!props.info.privacy && props.displayAllergens && (
+                    <div className="friend-delete-button">
+                        <p className="view-hide" onClick={viewDetails}>{dropdownState ? "Hide" : "View"} Allergens</p>
+                    </div>
+                    )}
+                    {dropdownState && (
+                        <div>
+                            {props.info.allergens && (
+                                <div className = "friends-allergen-block">
+                                    <p className="friends-allergen-list-title">Allergens:</p>
+                                    <div className="friends-allergen-list">{allergens}</div>
+                                </div>
+                            )}
+                            {!props.info.allergens && (
+                                <div className = "friends-allergen-block">
+                                    <p className="friends-allergen-list-title">Allergens: N/A</p>
+                                    <div className="friends-allergen-list"></div>
+                                </div>
+                            )}
+                        </div>
+                    )}
+                </div>
             </div>
-          )}
         </div>
-      )}
-      {/* <img src={userimg} id="userimg1" /> */}
-    </div>
-  );
+    );
+
 }
--- src/components/GroupCard.js
+++ src/components/GroupCard.js
@@ -1,69 +1,69 @@
 import React, { useState } from "react";
 
 function GroupMember(props) {
-  return (
-    <div id="group-member-card">
-      <div id="group-member-name">
-        <p>{props.member}</p>
-      </div>
-      <button
-        id="group-remove-user-button"
-        onClick={() => props.removeUser(props.group, props.member)}
-      >
-        <span>-</span>
-      </button>
-    </div>
-  );
+    return (
+        <div id="group-member-card">
+            <div id="group-member-name">
+                <p>{props.member}</p>
+            </div>
+            <button
+                id="group-remove-user-button"
+                onClick={() => props.removeUser(props.group, props.member)}
+            >
+                <span>-</span>
+            </button>
+        </div>
+    );
 }
 
 export default function GroupCard(props) {
-  const memberCards = Object.values(props.members).map((member, index) => {
+    const memberCards = Object.values(props.members).map((member, index) => {
+        return (
+            <GroupMember
+                key={index}
+                member={member}
+                group={props.name}
+                removeUser={props.removeUser}
+            />
+        );
+    });
+
+    const [dropdownState, setDropdownState] = useState(false);
+
+    const viewDropdown = () => {
+        setDropdownState(!dropdownState);
+    };
+
     return (
-      <GroupMember
-        key={index}
-        member={member}
-        group={props.name}
-        removeUser={props.removeUser}
-      />
-    );
-  });
-
-  const [dropdownState, setDropdownState] = useState(false);
-
-  const viewDropdown = () => {
-    setDropdownState(!dropdownState);
-  };
-
-  return (
-    <div id="group">
-      <div id="group-header">
-        <div id="group-title">
-          <h3>{props.name}</h3>
+        <div id="group">
+            <div id="group-header">
+                <div id="group-title">
+                    <h3>{props.name}</h3>
+                </div>
+                {!props.hideButtons && (
+                    <section id = "group-button-bar">
+                        <button
+                            id="group-add-user-button"
+                            onClick={() => {
+                                props.addUser(props.name, props.members);
+                            }}
+                        >
+                            <span>+</span>
+                        </button>
+                        <button id="group-dropdown-button" onClick={viewDropdown}>
+                            <span>{dropdownState ? "∨" : "∧"}</span>
+                        </button>
+                        <button
+                            id="group-delete-group-button"
+                            onClick={() => props.removeGroup(props.name)}
+                        >
+                            <span>x</span>
+                        </button>
+                    </section>
+                )}
+            </div>
+            {dropdownState && <div id="group-dropdown">{memberCards}</div>}
+            {/* {false && <AddUserToGroup members={props.members}/>} */}
         </div>
-        {!props.hideButtons && (
-          <section>
-            <button
-              id="group-add-user-button"
-              onClick={() => {
-                props.addUser(props.name, props.members);
-              }}
-            >
-              <span>+</span>
-            </button>
-            <button id="group-dropdown-button" onClick={viewDropdown}>
-              <span>^</span>
-            </button>
-            <button
-              id="group-remove-user-button"
-              onClick={() => props.removeGroup(props.name)}
-            >
-              <span>X</span>
-            </button>
-          </section>
-        )}
-      </div>
-      {dropdownState && <div id="group-dropdown">{memberCards}</div>}
-      {/* {false && <AddUserToGroup members={props.members}/>} */}
-    </div>
-  );
+    );
 }
